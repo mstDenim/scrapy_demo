0d33eb2196fed0598c8562f58e444091a33960b5
mstDenim <eismagieryt@gmail.com>
2025-12-05 00:24:32 +0100

    feat: added data accessed with xpaths

--- first_scraper/spiders/bookspider.py
+++ first_scraper/spiders/bookspider.py
@@ -9,11 +9,27 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response: Response):
         books = response.css("article.product_pod")
         for book in books:
-            yield{
-                'name' : book.css('h3 a').attrib['title'],
-                'price' : book.css('div p.price_color::text').get(),
-                'availability' : book.css('div p.instock.availability::text').get(),
-            }
+            individual_book = book.css('div a::attr(href)').get()
+            if individual_book is not None:
+                yield response.follow(individual_book, callback = self.parse_individual_bookpage)
+
         next_page = response.css('div li.next ::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, callback = self.parse)
+
+    def parse_individual_bookpage(self, response: Response):
+        book = response.css("article.product_page")
+        yield{
+            # why is the list in reverse order xD
+            # has to have something to do with 
+            # A: how -O in scrapy writes to a file 
+            # B: because of the way i call it here
+            'name' : book.css("div h1::text").get(),
+            'price' : book.css("div p.price_color::text").get(),
+            'product-description' : book.xpath('//div[@id="product_description"]/following-sibling::p/text()').get(),
+            'category' : book.xpath('//ul[@class = "breadcrumb"]/li[@class = "active"]/preceding-sibling::li[1]/a/text()')
+            .get(),
+
+
+        }
+        pass
